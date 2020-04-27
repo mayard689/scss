3b3ed1218e79599b83f5121a357d0ec143e2b3b2
mayard689 <doc-albert@laposte.net>
2020-04-27 23:53:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<title>MyPrecious.com</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="ring"></div>
+<div class="ring"></div>
+<div class="ring"></div>
+<div class="ring"></div>
+<div class="ring"></div>
+<div class="ring"></div>
+
+
+</body>
+</html>
+
